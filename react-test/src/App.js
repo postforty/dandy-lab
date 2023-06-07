ed739224eaad5b4163067d90c130fe4f9e750480
Dandy Code! <postforty@naver.com>
2023-06-07 11:12:41 +0900

    Update App.js

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -4,12 +4,7 @@ import MapVisible from "./components/MapVisible";
 function App() {
   return (
     <>
-      <>
-        <div>{/* <h1>Map Test</h1> */}</div>
-        {/* <MapTest /> */}
-        {/* <MapOptions /> */}
-        <MapVisible />
-      </>
+      <MapVisible />
     </>
   );
 }
