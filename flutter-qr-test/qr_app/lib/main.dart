c4059e4762518f1e78eef601d255cbb75ec2d3b2
Dandy Code! <postforty@naver.com>
2023-07-13 16:07:03 +0900

    [ADD] QR 생성 코드

--- flutter-qr-test/qr_app/lib/main.dart
+++ flutter-qr-test/qr_app/lib/main.dart
@@ -1,18 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static final ValueNotifier<ThemeMode> themeNotifier =
+      ValueNotifier(ThemeMode.light);
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      title: "Flutter App",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _qrGenerated = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter App'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _qrGenerated = !_qrGenerated;
+          });
+        },
+        child: const Icon(Icons.qr_code),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(15),
+              child: const Text("QR 코드 생성", style: TextStyle(fontSize: 20)),
+            ),
+            Container(
+              child: _qrGenerated
+                  ? QrImageView(
+                      data: "https://www.leotechkor.com/",
+                      version: QrVersions.auto,
+                      size: 200,
+                    )
+                  : const SizedBox.shrink(),
+            ),
+          ],
         ),
       ),
     );
